c3db83a3909c5fbc14d4855cb65c606ea2e0395d
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-17 07:33:50 -0600

    Add project files.

---
+++ NixonsAndDad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NixonsAndDad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. What is your name?");
+            var myName = Console.ReadLine();
+            Console.WriteLine($"Hey {myName} how are you?");
+            var response = Console.ReadLine();
+            Console.WriteLine($"Thats great to hear {myName} im glad your {response}.");
+            Console.WriteLine("What is your dogs name?");
+            var dogs = Console.ReadLine();
+            Console.WriteLine($"cool i like {dogs}");
+        }
+    }
+}
